e1964f38418fc2750f1c92e646e3a5955e1ca971
ezcanyigit <izgisezcan@gmail.com>
2022-11-17 11:21:37 +0300

    test

---
+++ main.py
@@ -0,0 +1,36 @@
+
+name = "Halit"
+age = "23"
+company = "kodlamio"
+print(f"{name} {age} yasinda {company} 'de calisiyor")
+
+
+text = "HABERİNİZ OLSUN"  #string 
+vade = 12    #integer
+FaizOrani = 1.47     #float 
+FaizOrani2 = 1.43
+
+
+print(text)
+print(vade)
+print(FaizOrani)
+print(FaizOrani2)
+print(type(text))
+print(type(vade))
+print(type(FaizOrani))
+
+mesaj = "Hoşgeldiniz"
+kullaniciAdi = "Ezcan"
+kullaniciSoyadi = "Yiğit"
+print(mesaj+kullaniciAdi+kullaniciSoyadi)
+print(mesaj+" "+kullaniciAdi+" "+kullaniciSoyadi)
+
+sayi1 = 10
+sayi2 = 20
+print(sayi1+sayi2)
+
+mesaj = "Hoşgeldiniz"
+kullaniciAdi = "Ezcan"
+kullaniciSoyadi = "Yiğit"
+sonucMesaj = (mesaj+" "+kullaniciAdi+" "+kullaniciSoyadi)
+print(sonucMesaj)
